a8208b86c08a952383b638ab352729ee8dbf15d5
ronaldoisc <tellezleal9928@gmail.com>
2022-02-07 23:08:06 -0600

    modify the approuter

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -6,6 +6,7 @@ import {
   // Navigate
 
 } from "react-router-dom";
+import { CharactersScreen } from '../containers/App/CharactersScreen';
 // import { CharactersScreen } from '../containers/App/CharactersScreen';
 // import { EpisodesScreen } from '../containers/App/EpisodesScreen';
 // import { CharacterProfileScreen } from '../containers/characters/CharacterProfileScreen';
@@ -15,7 +16,7 @@ import {
 export const AppRouter = () => {
   return <Router>
   <Routes>
-     <Route exact path='/' element={<h1>PLIS</h1>}/>
+     <Route exact path='/' element={<CharactersScreen/>}/>
      {/* <Route exact path='/character/:characterId' element={<CharacterProfileScreen/>}/> */}
      {/* <Route exact path='/episodes' element={<EpisodesScreen/>}/> */}
      {/* <Route path='/*' element={<Navigate to={'/characters'}/>} />  */}
