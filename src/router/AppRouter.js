d0c958cc8e8d9d3d90b6583270077512cdefc7d6
ronaldoisc <tellezleal9928@gmail.com>
2022-02-07 23:27:38 -0600

    rename the folder build

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -4,25 +4,23 @@ import {
   Routes,
   Route,
   Navigate,
-  // Navigate
+
 
 } from "react-router-dom";
 import { CharactersScreen } from '../containers/App/CharactersScreen';
 import { EpisodesScreen } from '../containers/App/EpisodesScreen';
 import { CharacterProfileScreen } from '../containers/characters/CharacterProfileScreen';
-// import { CharactersScreen } from '../containers/App/CharactersScreen';
-// import { EpisodesScreen } from '../containers/App/EpisodesScreen';
-// import { CharacterProfileScreen } from '../containers/characters/CharacterProfileScreen';
-
 
 
 export const AppRouter = () => {
   return <Router>
   <Routes>
-     <Route exact path='/' element={<CharactersScreen/>}/>
+     <Route exact path='/characters' element={<CharactersScreen/>}>
+
      <Route exact path='/character/:characterId' element={<CharacterProfileScreen/>}/>
+     </Route>
      <Route exact path='/episodes' element={<EpisodesScreen/>}/>
-     <Route path='/*' element={<Navigate to={'/'}/>} />
+     <Route path='/*' element={<Navigate to={'/characters'}/>} />
    
   </Routes>
 </Router>;
