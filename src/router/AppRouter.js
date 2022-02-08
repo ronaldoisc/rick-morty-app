bbf561ea659d1a4c917d366758a0a4a55f52ec89
ronaldoisc <tellezleal9928@gmail.com>
2022-02-07 23:42:02 -0600

    rename the folder

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -15,12 +15,12 @@ import { CharacterProfileScreen } from '../containers/characters/CharacterProfil
 export const AppRouter = () => {
   return <Router>
   <Routes>
-     <Route exact path='/' element={<CharactersScreen/>}/>
+     <Route exact path='/characters' element={<CharactersScreen/>}/>
 
      <Route exact path='/character/:characterId' element={<CharacterProfileScreen/>}/>
    
      <Route exact path='/episodes' element={<EpisodesScreen/>}/>
-     <Route path='/*' element={<Navigate to={'/'}/>} />
+     <Route path='/*' element={<Navigate to={'/characters'}/>} />
    
   </Routes>
 </Router>;
