b5e4cd24cbd70097860de99eb36135ead370ed7f
ronaldoisc <tellezleal9928@gmail.com>
2022-02-11 14:57:08 -0600

    change the name folder

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -4,20 +4,21 @@ import {
   Routes,
   Route,
   Navigate,
+  // Navigate,
 
 
 } from "react-router-dom";
 import { CharactersScreen } from '../containers/App/CharactersScreen';
-import { EpisodesScreen } from '../containers/App/EpisodesScreen';
-import { CharacterProfileScreen } from '../containers/characters/CharacterProfileScreen';
+// import { EpisodesScreen } from '../containers/App/EpisodesScreen';
+// import { CharacterProfileScreen } from '../containers/characters/CharacterProfileScreen';
 
 
 export const AppRouter = () => {
   return <Router>
   <Routes>
      <Route exact path='/rick-morty/' element={<CharactersScreen/>}/>
-     <Route exact path='/rick-morty/character/:characterId/' element={<CharacterProfileScreen/>}/>
-     <Route exact path='/rick-morty/episodes/' element={<EpisodesScreen/>}/>
+     {/* <Route exact path='/rick-morty/character/:characterId/' element={<CharacterProfileScreen/>}/> */}
+     {/* <Route exact path='/rick-morty/episodes/' element={<EpisodesScreen/>}/> */}
      <Route path='/rick-morty/*' element={<Navigate to={'/rick-morty/'}/>} />
   </Routes>
 </Router>;
